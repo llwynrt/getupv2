1921d096549f7bfebaca67a2c06e80d26d7087c9
llwynrt <marie-lyse.briffaud@shakertechnologies.com>
2016-01-21 12:30:27 +0100

    onglets colonne droite page session

--- session.php
+++ session.php
@@ -103,7 +103,7 @@
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-md-4">
-				GetUP e-commerce
+				GetUP e-commerce<br>Session prestige
 			</div>
 			<div class="col-md-2">
 				Angers
@@ -119,7 +119,7 @@
 			</div>
 		</div>
 
-		<div class="row"  style="background-color:#e0e0e0;">
+		<div class="row" style="background-color:#e0e0e0;">
     	  	<div class="col col-md-8">
     	  		<div class="row">
     	  			<div class="col-xs-12 onglet">
@@ -204,48 +204,110 @@
 						</div>
     					<div id="tarifs" class="bottom" style="display:block;">
     						<div class="row">
-    								<div class="col-xs-4 onglet">
-    									<div class="top" style="background-color:white;border-top:1px solid #D9DCE1;border-left:1px solid #D9DCE1;border-right:1px solid #D9DCE1;">
-    										Entrée
-    										à partir du 1er février 2016
-    										Hébergement en pension complète inclus dans le prix de la session<br>
-    										11 200€
-    									</div>
-    									<div class="bottom" style="background-color:#efaf42;">
-    										<div class="button white"><a href="#">J'en profite !</a></div>
-    									</div>
+   								<div class="col-md-4 onglet">
+   									<div class="top" style="background-color:white;border-top:1px solid #D9DCE1;border-left:1px solid #D9DCE1;border-right:1px solid #D9DCE1;">
+   										Entrée
+   										à partir du 1er février 2016
+   										Hébergement en pension complète inclus dans le prix de la session<br>
+   										11 200€
+   									</div>
+   									<div class="bottom" style="background-color:#efaf42;">
+   										<div class="button white" style=""><a href="#">J'en profite !</a>
+   										</div>
+   									</div>
+    							</div>
+   								<div class="col-md-4 onglet">
+   									<div class="top" style="background-color:white;border-top:1px solid #D9DCE1;border-left:1px solid #D9DCE1;border-right:1px solid #D9DCE1;">
+   										Early bird
+   										jusqu'au 31 janvier 2016
+   										4 places disponibles
+   										Hébergement en pension complète inclus dans le prix de la session<br>
+   										8 960€
+   									</div>
+   									<div class="bottom" style="background-color:#44b8ca;">
+   										<div class="button white"><a href="#">J'en profite !</a>
+   										</div>
+   									</div>
 
-    								</div>
-    								<div class="col-xs-4 onglet">
-    									<div class="top" style="background-color:white;border-top:1px solid #D9DCE1;border-left:1px solid #D9DCE1;border-right:1px solid #D9DCE1;">
-    										Entrée
-    										à partir du 1er février 2016
-    										Hébergement en pension complète inclus dans le prix de la session<br>
-    										11 200€
-    									</div>
-    									<div class="bottom" style="background-color:#44b8ca;">
-    										<div class="button white"><a href="#">J'en profite !</a></div>
-    									</div>
-
-    								</div>
-    								<div class="col-xs-4 onglet">
-    									<div class="top" style="background-color:white;border-top:1px solid #D9DCE1;border-left:1px solid #D9DCE1;border-right:1px solid #D9DCE1;">
-    										Entrée
-    										à partir du 1er février 2016
-    										Hébergement en pension complète inclus dans le prix de la session<br>
-    										11 200€
-    									</div>
-    									<div class="bottom" style="background-color:#e7525f;">
-    										<div class="button white"><a href="#">J'en profite !</a></div>
-    									</div>
-
-    								</div>
+   								</div>
+   								<div class="col-md-4 onglet">
+   									<div class="top" style="background-color:white;border-top:1px solid #D9DCE1;border-left:1px solid #D9DCE1;border-right:1px solid #D9DCE1;">
+   										Demandeurs d'emploi
+   										2 places disponibles
+   										Hébergement en pension complète inclus dans le prix de la session<br>
+   										8 245€
+   									</div>
+   									<div class="bottom" style="background-color:#e7525f;">
+   										<div class="button white"><a href="#">J'en profite !</a></div>
+   									</div>
+   								</div>
     						</div>
     					</div>
 					</div>
-
-      		</div>
-			</div>				
+      			</div>
+			</div>		
+			<div class="col col-md-4">
+				<div class="onglet">
+    				<div class="top">
+						<h2>Où ?</h2>
+					</div>
+    				<div class="bottom" style="border:1px solid #C9D1D4;">
+						<div id="map" style="height:200px;">
+						</div>
+						<script>
+							function initMap() {
+							var myLatLng = {lat: 47.5016125, lng: -0.3530603};
+							var map = new google.maps.Map(document.getElementById('map'), {
+								zoom: 10,
+								center: myLatLng
+							});
+							var marker = new google.maps.Marker({
+								position: myLatLng,
+								map: map,
+								title: 'Château de Briançon'
+							});
+						}
+						</script>
+						<script async defer
+						    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDxC0NzZSbf4W7-H3LX7v6bzEQ8nNiKY3Q&signed_in=true&callback=initMap">
+						</script>
+						Le site d'exception du château de Briançon, à 2h30 de Paris, en Maine et Loire, vous accueille.<br>
+						Château de Briançon, (départementale 116)<br>
+						49140 Beauné, France<br>
+						<a href="http://www.chateau-de-briancon.com/fr/">http://chateau-de-briancon.com</a>
+					</div>
+				</div>
+				<div class="onglet">
+    				<div class="top">
+						<h2>Comment venir ?</h2>
+					</div>
+    				<div class="bottom" style="border:1px solid #C9D1D4;">
+    					<ul><li>Train :</li><li>Voiture :</li></ul>
+					</div>
+				</div>
+				<div class="onglet">
+    				<div class="top" style="background-color:#e7525f;">
+						<h2>Besoin d'aide ?</h2>
+					</div>
+    				<div class="bottom" style="border:1px solid #C9D1D4;">
+    					07 68 010 456<br>
+    					Infos et réservations<br>
+    					du lundi au samedi de 9h à 21h<br>
+    					contact@getup-startup.com
+					</div>
+				</div>
+				<div class="onglet">
+    				<div class="top">
+						<h2>Agenda</h2>
+					</div>
+    				<div class="bottom" style="border:1px solid #C9D1D4;">
+    					<iframe 
+    						src="https://www.google.com/calendar/embed?showTitle=0&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=k8sog225cbf3afui8h1k72lf58%40group.calendar.google.com&amp;ctz=Europe%2FParis" 
+					    	style=" border-width:0 " width="100%" height="300" frameborder="0" scrolling="no">
+					    </iframe>
+				</div>
+				
+			</div>		
 		</div>
       	<!-- /.row -->
       	<section class="row">
