c502d6ba2c51207b1f79a3ab2c357e7c314c0c03
llwynrt <marie-lyse.briffaud@shakertechnologies.com>
2016-01-21 12:31:56 +0100

    onglets colonne droite page session 2

---
+++ session_home.php
@@ -0,0 +1,372 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Marie-Lyse Briffaud">
+
+    <title>GetUP - Run your business</title>
+
+    <!-- Bootstrap Core CSS -->
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+	<link href="assets/css/bootstrap_ms.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="assets/css/style.css" rel="stylesheet">
+
+    <link rel="icon" type="image/png" href="favicon.png" />
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	
+	<style>
+		/*.row{border:1px solid black;margin-left:0px;margin-right:0px;}
+		.col{border:1px solid red;}*/
+
+		
+	</style>
+
+</head>
+
+<body data-grid-framework="b3f" data-grid-color="gray" data-grid-opacity="0.3" data-grid-zindex="10" data-grid-gutterwidth="30px" data-grid-nbcols="12">
+	<div class="container-fluid">
+		<div class="row wide">
+			<div class="col col-md-2 hidden-sm hidden-ms hidden-xs">
+				<a id="home" href="#"><img src="assets/images/logo-getup-v2_xs.png" alt="">
+					<h1 style="display:none;">GetUP : Run your business</h1>
+				</a>
+			</div>
+			<div class="col col-lg-8 col-md-7 col-sm-9">
+				<!-- Navigation -->
+				<nav class="navbar navbar-default">
+					<div class="container-fluid">
+
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							
+						</div>
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1" style="padding-left:0px;">
+							<ul class="nav nav-justified">
+								<li>
+									<a href="#">A Propos</a>
+								</li>
+								<li>
+									<a href="#">Sessions</a>
+								</li>
+								<li>
+									<a href="#">Agenda</a>
+								</li>
+								<li>
+									<a href="#">Organiser</a>
+								</li>
+								<li>
+									<a href="#">Contact</a>
+								</li>
+					   
+							</ul>
+						</div>
+						<!-- /.navbar-collapse -->
+			
+					</div>
+					<!-- /.container -->
+			
+				</nav>
+			</div>
+			<div class="col col-lg-2 col-md-3 col-sm-3 hidden-ms hidden-xs text-center">         
+				<div id="participer" style="height:60px;padding-top:10px;">
+                    <a href="#"><span style="text-transform: uppercase;font-family:'Montserrat-Bold';height:200px;">Participer</span><br />à une session GetUP</a>
+                </div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="col col-lg-12" id="banniere">
+	</div>
+
+    
+	<!-- Page Content -->
+	<div class="container-fluid">       
+		<section class="row">
+    	  	<div class="col col-md-8 col col-sm-8" id="presentation">
+				<h2>Vous lancez votre entreprise ?</h2>
+				<p class="chapeau">Nous vous proposons de cristaliser votre model business, 
+				créer votre site, le tester, identifier le profil de vos premiers clients 
+				et se lancer sur le marché, au bout d'une semaine seulement.</p>
+				<div class="button blue"><a href="#">Découvrir la session</a></div>
+			</div> 
+        	<div class="col col-md-3 col col-md-offset-1 col col-sm-4 col-ms-12 text-center">
+				<aside>
+					<h3>Session mars : -20% sur les préventes</h3>
+					Jusqu'au 31 janvier.<br /><del>11 200€</del> 8 920€
+					<div class="button white"><a href="#">J'en profite !</a></div>
+				</aside>
+			</div>
+		</section>
+		<!-- /.row -->
+
+      	<section class="row" id="session">
+      		<div class="col col-lg-12 text-center">
+				<h2>Ce que j'obtiens durant la session</h2>
+				<p class="chapeau">
+				Nous vous proposons de cristaliser votre model business, créer votre site.</p>
+			</div>
+			<div class="col col-md-6">
+				<div class="row">
+					<div class="col-xs-12">
+						<div class="carre carre-top" id="business">
+							<h3><span class="titre1">L'agilité d'entreprise</span><br><span class="titre2">Avec un professionnel du lean startup</span></h3>
+						   	<ul>
+						   		<li>&bull; Je découvre comment dépenser moins pour tester rapidement mon business model sans me mettre en danger financièrement.</li>
+								<li>&bull; Je réalise avec le groupe mon business model canvas. </li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-xs-12">
+						<div class="carre carre-top" id="design">
+							<h3><span class="titre1">Une charte graphique adaptée à mes besoins</span><br><span class="titre2">Avec un professionnel du graphisme</span></h3>
+							<ul><li>&bull; Le web designer réalise ma charte graphique incluant un logo simple, mes couleurs et mes typographies.</li>
+								<li>&bull; Il m'apprend les règles de base de la communication graphique, les erreurs à éviter. </li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col col-md-6">
+				<div class="carre carre-top" id="cdc">
+					<h3><span class="titre1">Mon cahier des charges et un site e-commerce</span><br><span class="titre2">Avec un professionnel des projets web</span></h3>
+					<ul>
+						<li>&bull; Comment exprimer mes besoins et, grâce aux méthodes agiles, comment éviter les principaux écueils qui font échouer les projet. </li>
+						<li>&bull; Je réalise mon cahier des charges avec des outils simples.</li>
+						<li>&bull; Il m'apprend à utiliser le backoffice du système de gestion de contenu et comment mettre mon site en ligne.</li>
+						<li>&bull; Je découvre les bases du développement internet avec les langages HTML et CSS.</li>
+						<li>&bull; Je suis capable de faire évoluer le contenu et l'apparence de mon site.</li>
+						<li>&bull; Je connais les bases du référencement internet et de la rédaction de contenu optimisé pour Google.</li>
+					<ul>
+				</div>
+			</div>
+			<div class="col-md-12">
+				<div class="row carre-large-h">
+					<div class="col col-md-6 carre-large-col">
+						<div class="carre carre-top" id="commerce">
+							<h3><span class="titre1">Résultat</span><br><span class="titre2">Mon site e-commerce simple est en ligne !</span></h3>
+							<ul>
+								<li>&bull; Le prix inclut le nom de domaine et l'hébergement de mon site pour un an. Je n'ai rien d'autre à payer.</li>
+								<li>&bull; Je suis autonome : je sais ajouter du contenu et réaliser des modifications simples. En cas de besoin, je peux faire appel à des professionnels parmi les meilleurs du secteur pour m'aider à perfectionner mon site.</li>
+								<li>&bull; J'ai appris les bases du code informatique.</li>
+								<li>&bull; Je comprends les enjeux de la création de site internet et du développement de mon entreprise.</li>
+							<ul>
+						</div>
+					</div>
+					<div class="col col-md-6 carre-large-col text-center">
+						<div class="carre" id="conclusion">
+							<p>Parce que j'ai trouvé rassemblés en un même lieu et un même temps, toutes les compétences nécessaires à la réussite de mon projet web.<br>Parce que j'ai découvert et mis en oeuvre une méthode agile (Scrum).</p>
+							<p>Et surtout : du concret, rapidement.<br>J'ai gagné un temps précieux.<br>Et ça, ça n'a pas de prix. </p>
+						</div>
+					</div>
+				</div>
+			</div>				
+		</section>
+      	<!-- /.row -->
+
+
+
+      	<section class="row" style="padding-bottom:20px;">
+      		<div class="col col-lg-12 text-center">
+				<h2>Les intervenants</h2>
+				<p class="chapeau">
+				Réalisez votre site web avec une équipe d'experts.</p>
+			</div>
+			<div class="col col-sm-4 col-ms-4">
+				<div class="row">
+				<div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+					<img class="img-circle img-responsive img-center" src="assets/img/oana.jpg" alt="">
+				</div>
+				<div class="col-lg-12 text-center">
+					Oana Juncu<br>coach en agilité & lean startup
+				</div>
+				</div>
+			</div>
+			<div class="col col-sm-4 col-ms-4">
+				<div class="row">
+				<div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+					<img class="img-circle img-responsive img-center" src="assets/img/mickael.jpg" alt="">
+				</div>
+				<div class="col-lg-12 text-center">
+					Mickaël Ruau<br>gestionnaire de projets web
+				</div>
+				</div>
+			</div>
+			<div class="col col-sm-4 col-ms-4">
+				<div class="row">
+				<div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+					<img class="img-circle img-responsive img-center" src="assets/img/indiens.jpg" alt="">
+				</div>
+				<div class="col-lg-12 text-center">
+					Alice & Angélique<br>
+					Les Indiens, design graphique
+				</div>
+				</div>
+			</div>
+			<!--</div>-->
+		</section>
+      	<!-- /.row -->
+
+      	<section class="row">
+      		<div class="col col-lg-12 text-center">
+      			<h2>Un lieu exceptionnel</h2>
+      			<p class="chapeau">L'hébergement en pension complète est compris dans le prix de la session.</p>
+      		</div>
+
+			<div class="col-md-12">
+				<div class="row carre-large-h" style="background-color:white;">
+		
+					<div class="col col-sm-6 carre-large-col">
+						<div class="carre" style="margin:0px;padding-left:20px; padding-top:0px;">
+							<h3>Le château de Briançon</h3>
+							<p style="font-family:'Roboto';font-weight:lighter;font-size:18px;">en Maine et Loire, à 2h30 de Paris</p>
+							<p style="font-family:'Roboto';font-weight:lighter">Venez passer une semaine sur le site exceptionnel du château de Briançon pour réaliser votre site e-commerce avec une équipe d'experts.</p>
+							<p style="font-family:'Roboto';font-weight:bold">Château de Briançon, (départementale 116)<br>49140 Beauné, France<br>
+							<a href="http://www.chateau-de-briancon.com/fr/">http://www.chateau-de-briancon.com</a></p>
+							<div class="button blue"><a href="#decouvrir">Découvrir la session</a></div>
+
+						</div>
+					</div>
+					<div class="col col-sm-6" style="padding-left:0px;padding:0px;border-top-right-radius:20px;	border-bottom-right-radius:20px;">
+						<div class="carre" style="margin:0px;border-top-left-radius: 0px;border-bottom-left-radius: 0px;padding:0px;">
+							<img src="assets/images/chateau3.jpg" alt="" style="width:100%;border-top-right-radius:20px;border-bottom-right-radius:20px;">
+						</div>
+					</div>
+				</div>
+			</div>
+
+
+      	</section>
+
+      	<section class="row">
+      		<div class="col col-lg-12 text-center" style="margin-bottom:50px;">
+				<h2>Partenaires</h2>
+				<img src="assets/images/partenaires/agilumens.png" alt="logo agilumens"><img src="assets/images/partenaires/shaker.png" alt="logo shaker">
+			</div>
+		</section>
+		<!-- /.row -->
+
+		<div class="row" id="footer">
+			<div class="col col-md-4 text-center" id="logo">
+				<img src="assets/images/logo-getup-v2_sm-inv.png" alt="">
+			</div>
+			<div class="col col-md-4 text-left" style="color:white;">
+				
+				<!--<div class="row">
+					<div  id="menu" class="col col-lg-12">-->
+						<div id="menu" >
+							<ul>
+								<li>A propos</li>
+								<li>La méthode</li>
+								<li>Le concept</li>
+								<li>Il parlent de nous</li>
+							</ul>
+							<ul>
+								<li>Sessions</li>
+								<li>E-commerce</li>
+								<li>Site vitrine</li>
+								<li>E-commerce prestige</li>
+							</ul>
+							<ul>
+								<li>Agenda</li>
+							</ul>
+							<ul>
+								<li>Organiser</li>
+								<li>Créer une session</li>
+								<li>Intervenir</li>
+								<li>Suggérer une session</li>
+								<li>Améliorer GetUP</li>
+							</ul>
+							<ul>
+								<li>Contact</li>
+							</ul>
+						</div>
+					<!--</div>-->
+
+					<div id="reseaux" class="text-center">
+						Rejoignez-nous 
+						<ul>
+							<li>
+								<a href="https://twitter.com/GetUpBiz">
+									<img src="assets/images/picto1.png" alt="">
+								</a>
+							</li>
+							<li>
+								<a href="https://www.facebook.com/getupbiz/">
+									<img src="assets/images/picto2.png" alt="">
+								</a>
+							</li>
+						</ul>
+					</div>
+				<!--</div>-->
+			</div>
+			<div class="col col-md-4 text-center">
+				<div id="twitter">
+						<h2>Suivez notre aventure !</h2>
+						<a class="twitter-timeline"  href="https://twitter.com/hashtag/GetUpBiz" data-widget-id="641631327851315200" data-chrome="noheader transparent">
+							Tweets sur #GetUpBiz
+						</a>
+						<script>
+							!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+						</script>
+				</div>
+			</div>
+		</div>
+		<!-- /.row -->
+
+		<div class="row wide" id="liens">
+			<div class="col col-sm-3 col col-ms-2 text-center">
+				© GetUP 2016
+			</div>
+			<div class="col col-sm-6 col col-ms-8 text-center">
+				<ul><li><a href="mentions.html">Mentions Légales</a></li></ul>
+			</div>
+		</div>
+		<!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+
+  	<!-- CDN jQuery -->
+	<script src="assets/js/jquery.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" crossorigin="anonymous">
+	</script>
+	
+	<script>
+	//défilement doux
+		$(function () {
+			$('.button>a').on('click', function(e) {
+				e.preventDefault();
+				var hash = this.hash;
+				$('html, body').animate({
+					scrollTop: $(this.hash).offset().top
+				}, 1000, function(){
+					window.location.hash = hash;
+				});
+			});
+		});
+	</script>
+
+</body>
+
+</html>
